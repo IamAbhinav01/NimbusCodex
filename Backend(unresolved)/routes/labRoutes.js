fa4be507c31299f4dfd47900b35496d2523d7f9f
Adithya Damodara <142497912+Adithyadamodara@users.noreply.github.com>
2026-03-14 04:24:27 +0530

    Delete Backend(unresolved) directory

--- Backend(unresolved)/routes/labRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const labController = require("../controllers/labController");
-
-router.post("/launch", labController.launchLab);
-
-module.exports = router;
